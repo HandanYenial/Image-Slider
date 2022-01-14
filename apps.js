dbdd516a1c1aa24f132d16ed65fa30979352c5df
Handan Yenial <88174651+HandanYenial@users.noreply.github.com>
2022-01-14 09:41:57 -0500

    Update apps.js

--- apps.js
+++ apps.js
@@ -6,3 +6,8 @@ function showImage(i){
     index+=1;
 
     let images=document.getElementsByClassName("image");
+
+     let dots= document.getElementsByClassName("dot");
+
+    for(i=0; i<images.length; i++)
+    images[i].getElementsByClassName.display="none";
